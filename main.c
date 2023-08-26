b76878312487e3037061d95d06e7f303d910128a
Nick Sun <nick_sun@asus.com>
2023-08-26 11:14:30 +0800

    change main.c

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 int main (int argc, char **argv)
 {
-    printf ("[develop branch]Hello Yocto!\n");
+    printf ("[main branch]Hello Yocto!\n");
     printf ("I am learn-yocto project\n");
 
     return 0;
